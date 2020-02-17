ed0762c320f64cde021645a621d4271d8d1f4cef
SwimmingLee <swimming_7291@daum.net>
2020-02-17 14:34:40 +0900

    [feat] backend update

--- back/routes.js
+++ back/routes.js
@@ -13,8 +13,9 @@ const UPDATE_PASSWORD = "/pass"
 const JOINED_STUDY = "/joined-study";
 const LEAVE_STUDY = "/leave-study"
 
-// Images
+// Images & Files
 const IMAGES = "/images"
+const FILES = "/files"
 
 // Study
 const STUDIES = "/studies";
@@ -24,6 +25,7 @@ const STUDY_MARK = "/mark";
 const STUDY_DETAIL = "/detail";
 const STUDY_APPLY = "/apply"
 const STUDY_JOIN = "/join"
+const CHANGE_LEVEL = "/change-level"
 
 // Post
 const POSTS = "/posts"
@@ -50,7 +52,13 @@ const APPLY_DETAIL = "/detail";
 const ALARMS = "/alarms";
 const ALARM = "/create";
 const ALARM_DETAIL = "/detail";
-const ALARM_NUMBER = "/number"
+const ALARM_NUMBER = "/number";
+
+//email
+const EMAIL = "/email"
+const AUTH = "/auth"
+const NOTICE_APPLY = "/notice_apply"
+const RESULT_APPLY = "/result_apply"
 
 const routes = {
     home : HOME,
@@ -67,6 +75,7 @@ const routes = {
     leave_study : LEAVE_STUDY,
 
     images : IMAGES,
+    files : FILES,
     
     studies : STUDIES,
     study : STUDY,
@@ -75,6 +84,7 @@ const routes = {
     study_mark : STUDY_MARK,
     study_apply: STUDY_APPLY,
     study_join : STUDY_JOIN,
+    change_level : CHANGE_LEVEL,
 
     posts : POSTS,
     post_number: POST_NUMBER,
@@ -96,6 +106,11 @@ const routes = {
     alarms : ALARMS,
     alarm : ALARM,
     alarm_detail : ALARM_DETAIL,
-    alarm_number : ALARM_NUMBER
+    alarm_number : ALARM_NUMBER,
+
+    email:EMAIL,
+    auth:AUTH,
+    notice_apply : NOTICE_APPLY,
+    result_apply : RESULT_APPLY,
 }
 export default routes;
