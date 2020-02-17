134ff8d2ae6f06dc51d181d6f0f4ade798caf67f
cyun <dawnstar1007@naver.com>
2020-02-17 18:22:19 +0900

    modified

--- front/src/components/workspace/WorkSpace.vue
+++ front/src/components/workspace/WorkSpace.vue
@@ -176,19 +176,12 @@ export default {
     Chatting: Chatting
   },
   beforeCreate() {
-<<<<<<< HEAD
     if (!window.opener) {
       this.$router.push({ name: "home" });
     }
   },
   created() {
     if (!window.opener) return;
-=======
-      // if (!window.opener) {this.$router.push({name : 'home'})}
-  },
-  created() {
-    // if (!window.opener) return
->>>>>>> 7dda99c615654520c1ce6c8d12e47799df951bd6
     this.user = this.debuging
       ? {
           user_id: `${Math.ceil(40 + Math.random() * 40)}`,
@@ -217,17 +210,10 @@ export default {
   mounted() {
     window.moveTo(0, 0);
     window.resizeTo(screen.availWidth, screen.availHeight + 100);
-<<<<<<< HEAD
 
     if (!window.opener) return;
     window.onkeyup = event => {
       if (event.keyCode == 27) {
-=======
-    
-    // if (!window.opener) return
-    window.onkeyup = (event)=>{
-      if(event.keyCode==27){
->>>>>>> 7dda99c615654520c1ce6c8d12e47799df951bd6
         this.overlay = false;
       }
     };
